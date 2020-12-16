7c375ba611e1e2520fb10792f8897b6092c576f3
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2020-12-16 19:06:09 +0100

    fixing some errors

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,58 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Dashboard () {
+  const [user, setUser] = useState({})
+  const handleClick = (e) => {
+    e.preventDefault();
+    // API goes here
+  }
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUser({
+      ...user,
+      [e.target.name]: value
+    })
+  }
     return (
         <>
-        <div class="flex items-center justify-center mt-96">
-        <div class="w-full max-w-md">
-          <form class="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4">
+        <div className="flex items-center justify-center mt-96">
+        <div className="w-full max-w-md">
+          <form className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4">
             <div
-              class="text-gray-800 text-2xl flex justify-center border-b-2 py-2 mb-4"
+              className="text-gray-800 text-2xl flex justify-center border-b-2 py-2 mb-4"
             >
               Dashboard Login
             </div>
-            <div class="mb-4">
+            <div className="mb-4">
               <label
-                class="block text-gray-700 text-sm font-normal mb-2"
-                for="username"
+                className="block text-gray-700 text-sm font-normal mb-2"
+                htmlFor="username"
               >
                 Username
               </label>
               <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
-                autofocus
+                autoFocus
+                name="username"
                 placeholder="Username"
+                onChange={handleChange}
               />
             </div>
-            <div class="mb-6">
+            <div className="mb-6">
               <label
-                class="block text-gray-700 text-sm font-normal mb-2"
-                for="password"
+                className="block text-gray-700 text-sm font-normal mb-2"
+                htmlFor="password"
               >
                 Password
               </label>
               <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 v-model="form.password"
                 type="password"
                 placeholder="Password"
                 name="password"
                 required
-                autocomplete="current-password"
+                autoComplete="current-password"
+                onChange={handleChange}
               />
             </div>
-            <div class="flex items-center justify-between">
-              <button class="px-4 py-2 rounded text-white inline-block shadow-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-700" type="submit">Sign In</button>
-              <a
-                class="inline-block align-baseline font-normal text-sm text-blue-500 hover:text-blue-800"
-                href="#"
-              >
-                Forgot Password?
-              </a>
+            <div className="flex items-center justify-between">
+              <button className="px-4 py-2 rounded text-white inline-block shadow-lg bg-blue-500 hover:bg-blue-600 focus:bg-blue-700" onClick={handleClick}>Sign In</button>
             </div>
           </form>
-          <p class="text-center text-gray-500 text-xs">
+          <p className="text-center text-gray-500 text-xs">
             &copy;2020 atah1r1. All rights reserved.
           </p>
         </div>
