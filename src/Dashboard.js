834cfa1d0c1c136f04744a749f36c06ef438bfb4
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2020-12-17 19:46:48 +0100

    adding logout function

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 const Dashboard = () => {
+    const logoutFunc = () => {
+        axios({
+            url: '/api/logout/',
+            method: "post",
+          }).then(
+            response => {
+                if (response.status === 200){
+                    window.location.href = '/login/';
+                }
+            })
+    }
     return (
-        <div>salam</div>
+        <button onClick={logoutFunc}>Logout</button>
     )
 }
 
