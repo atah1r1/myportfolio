117de2749b70d1b6db9199d59bf416a66c850560
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2020-12-22 20:24:14 +0100

    adding profile in dashboard page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,8 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+/* 
+  "full_name": null,
+    "avatar": null,
+    "mini_about": null,
+    "about": null,
+    "born_date": null,
+    "address": null,
+    "phone": null,
+    "email": null,
+    "github": null,
+    "linkedin": null,
+    "facebook": null,
+    "twitter": null,
+    "instagram": null
+*/
 
 const Dashboard = () => {
-    const [connected, setConnected] = useState(null);
+    const [connected, setConnected] = useState(true /*null*/);
     const logoutFunc = () => {
         axios({
             url: '/api/logout/',
@@ -25,19 +40,19 @@ const Dashboard = () => {
     return (
         <>
         { connected ? <main class="flex w-full h-screen">
-<aside class="w-80 h-screen bg-gray shadow-md w-fulll hidden sm:block">
+<aside class="w-80 h-screen bg-gray shadow-md hidden sm:block">
   <div class="flex flex-col justify-between h-screen p-4 bg-gray-800">
       <div class="text-sm">
-        <div class="bg-gray-900 text-white p-5 rounded cursor-pointer">Teams in space</div>
-        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Backlog</div>
-        <div class="bg-gray-700 text-blue-300 p-2 rounded mt-2 cursor-pointer">Board</div>
+        <div class="bg-gray-900 text-white p-5 rounded">Dashboard</div>
+        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Profile</div>
+        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Projects</div>
+        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Competences</div>
+        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Experiences</div>
+        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Educations</div>
         <div class="bg-gray-900 flex justify-between items-center text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
-          <span>Reports</span>
+          <span>Messages</span>
           <span class="w-4 h-4 bg-blue-600 rounded-full text-white text-center font-normal text-xs">5</span>
         </div>
-        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Releases</div>
-        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Components</div>
-        <div class="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Repository</div>
       </div>
 
       <div class="flex p-3 text-white bg-red-500 rounded cursor-pointer text-center text-sm" onClick={logoutFunc}>
@@ -48,11 +63,76 @@ const Dashboard = () => {
       </div>
   </div>
 </aside>
+<section class="w-full">
+    <div class="bg-transparent min-h-screen pt-2 font-mono">
+        <div class="container mx-auto">
+            <div class="inputs w-full max-w-2xl p-6 mx-auto">
+                <h2 class="text-2xl text-gray-900">Account Setting</h2>
+                <form class="mt-6 pt-4">
+                        <div class="personal w-full border-t border-gray-400 pt-4">
+                            <h2 class="text-2xl text-gray-900">Personal info:</h2>
+                            <div class="flex items-center justify-between mt-4">
+                                <div class='w-full md:w-1/2 px-3 mb-6'>
+                                    <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >first name</label>
+                                    <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                                </div>
+                                <div class='w-full md:w-1/2 px-3 mb-6'>
+                                    <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >last name</label>
+                                    <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                                </div>
+                            </div>
+                            <div class='w-full md:w-full px-3 mb-6'>
+                                <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>user name</label>
+                                <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                            </div>
+                            <div class='w-full md:w-full px-3 mb-6'>
+                                <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >Mini about</label>
+                                <textarea class='bg-gray-100 rounded-md border text-gray-700 leading-normal resize-none w-full h-15 py-2 px-3 shadow-inner border border-gray-400 font-medium placeholder-gray-700 focus:outline-none focus:bg-white'  required></textarea>
+                            </div>
+                            <div class='w-full md:w-full px-3 mb-6'>
+                                <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >Bio</label>
+                                <textarea class='bg-gray-100 rounded-md border text-gray-700 leading-normal resize-none w-full h-32 py-2 px-3 shadow-inner border border-gray-400 font-medium placeholder-gray-700 focus:outline-none focus:bg-white'  required></textarea>
+                            </div>
+                            <div class='w-full md:w-full px-3 mb-6'>
+                                <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Address</label>
+                                <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                            </div>
+                            <div class='w-full md:w-full px-3 mb-6'>
+                                <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Phone</label>
+                                <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                            </div>
+                            <div class='w-full md:w-full px-3 mb-6'>
+                                <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'>Email</label>
+                                <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                            </div>
+                            <div class="flex items-center justify-between mt-4">
+                                <div class='w-full md:w-1/4 px-3 mb-6'>
+                                    <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >Github</label>
+                                    <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                                </div>
+                                <div class='w-full md:w-1/4 px-3 mb-6'>
+                                    <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >Linkedin</label>
+                                    <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                                </div>
+                                <div class='w-full md:w-1/4 px-3 mb-6'>
+                                    <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >Instagram</label>
+                                    <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                                </div>
+                                <div class='w-full md:w-1/4 px-3 mb-6'>
+                                    <label class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2' >Twitter</label>
+                                    <input class='appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500' type='text'  required></input>
+                                </div>
+                            </div>
+                            <div class="flex p-3 text-white bg-red-500 rounded cursor-pointer text-center text-sm">
+                                <button class="rounded inline-flex items-cente" type="submit">save changes</button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
-<section class="w-full p-4">
-  <div class="w-full h-64 border-dashed border-4 p-4 text-md">Dashboard</div>
   </section>
-
 </main> : <div>You are not allowed to see this page!</div>
         }
 </>
