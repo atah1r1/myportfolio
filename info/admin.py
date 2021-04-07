2aafbb4fcfb874b101abff76e6bdac2c1aa90df3
Amine Tahiri <atahiri@student.1337.ma>
2021-04-07 17:56:08 +0100

    sdfsdf

--- info/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Information)
-admin.site.register(Project)
-admin.site.register(Message)
-admin.site.register(Education)
-admin.site.register(Competence)
-admin.site.register(Experience)
--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ function App() {
           <Copyright />
       </Route>
     </>
+	////ssfsfsdf
   )
 }
 
